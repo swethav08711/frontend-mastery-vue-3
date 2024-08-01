d3ec66e636730591aaaa430c72581748242fad57
swethav08711 <shwe08711@gmail.com>
2024-08-01 21:42:11 +0530

    v-model

---
+++ vue-3-learning/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>v-modal</h1>
+    <VModal />
+  </div>
+</template>
+<script setup>
+import VModal from "./components/VModal.vue";
+</script>
+<style scoped></style>
---
+++ vue-3-learning/src/components/VModal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <textarea v-model="dynamicMessage" />
+    <p>{{ dynamicMessage }}</p>
+  </div>
+  <hr />
+
+  <div>
+    <!-- <input type="checkbox" value="vuejs" v-model="checkboxOptions" />
+    <label for="vuejs">vuejs</label>
+    <input type="checkbox" value="react" v-model="checkboxOptions" />
+    <label for="react">react</label>
+    <input type="checkbox" value="html" v-model="checkboxOptions" />
+    <label for="html">html</label> -->
+
+    <div v-for="option in options" :key="option.name">
+      <input type="checkbox" :value="option.name" v-model="checkboxOptions"  :id="option.value"/>
+      <label for="option.name">{{ option.name }}</label>
+    </div>
+
+    <span>{{ checkboxOptions }}</span>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const dynamicMessage = ref(null);
+dynamicMessage.value = "Thsi is a mead";
+
+const checkboxOptions = ref([]);
+const options = [
+  {
+    name: "vuejs",
+  },
+  {
+    name: "react",
+  },
+  {
+    name: "html",
+  },
+];
+</script>
+
+<style scoped></style>
